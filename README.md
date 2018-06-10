0557d84bf93eb97ef69694d98557e240689b4883
unk900t2 <stanislav.savolainen@mail.com>
2018-06-10 22:20:15 +0300

    hello-world reactJS installation script is now available for Windows - platform

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # React "hello world"-project installation script
 
+
+For Linux run shell - script ( react2018_proj_setup.sh )
+
+For Windows run bath - script ( react_proj_setup.bat ) 
+
+
+
+------------------------------------------------------------------------------------------------------------------------
+
 If react_proj_setup.sh don't generate bundle.js file, then you can use react2018_proj_setup.sh.
 
 There is something changed in babel - modules installation from internet what cause my old scrit stop generating bundle.js file.
@@ -18,15 +27,18 @@ src/client/index.jsx  ( react code here )
 
 then write following command on Linux terminal to "compile" react to old javascript:
 
-./node_modules/.bin/webpack -d
+ ```js ./node_modules/.bin/webpack -d  ```
 
+alson write following command on Windows command line (cmd) to "compile :
+
+```js  npm run-script build  ```js
 
 This cause src/client/public/bundle.js - file update, after that you can open your project in browser at src/client/index.html.
 Remember that after compilation you can see changes in browser, because index.html read bundle.js file. This mean that if you need
 to host project you need only index.html and bundle.js files. Other files are for development.
 
 
-
+Please note that NodeJS have to be installed for both script !
 
 
 
---
+++ react_proj_setup.bat
@@ -0,0 +1,134 @@
+@echo off
+rem -------> this script done by Stanislav Savolainen <--------------------------
+rem  ------> build react.js "hello world" - applicaion on windows - platform <--- 
+rem  ------> script require NodeJs to be installed (using npm ) <---------------- 
+rem -----------------------------------------------------------------------------
+echo "remove project files if exsist"
+del package.json
+del webpack.config.js
+rem https://stackoverflow.com/questions/7331056/how-to-delete-a-folder-with-all-contents-using-a-bat-file-in-windows
+@RD /S /Q "src"
+@RD /S /Q "node_modules"
+del .babelrc
+timeout 5
+rem -----------------------------------------------------------------------------
+echo "Create package.json"
+echo {  >> package.json
+echo   "name": "client",  >> package.json
+echo  "version": "1.0.0",  >> package.json
+echo   "description": "",  >> package.json
+echo   "main": "index.js",  >> package.json
+echo  "scripts": {  >> package.json
+echo     "test": "echo \"Error: no test specified\" && exit 1",  >> package.json
+echo	  "dev": "webpack -d --watch", >> package.json
+echo      "build": "webpack -p" >> package.json
+echo   },  >> package.json 
+echo   "author": "",  >> package.json
+echo   "license": "ISC",  >> package.json
+echo   "dependencies": {  >> package.json
+echo     "babel-core": "6.26.0",  >> package.json
+echo     "babel-loader": "7.1.2",  >> package.json
+echo     "babel-preset-es2015": "6.24.1",  >> package.json
+echo     "babel-preset-react": "6.24.1",  >> package.json
+echo     "react": "16.0.0",  >> package.json
+echo     "react-dom": "16.0.0",  >> package.json
+echo     "webpack": "3.6.0"  >> package.json
+echo   },  >> package.json
+echo   "devDependencies": {  >> package.json
+echo     "babel-plugin-transform-class-properties": "6.24.1",  >> package.json
+echo     "babel-plugin-transform-object-rest-spread": "6.26.0"  >> package.json
+echo   }  >> package.json
+echo }  >> package.json
+rem -----------------------------------------------------------------------------
+echo var webpack = require('webpack'); >> webpack.config.js
+echo var path = require('path'); >> webpack.config.js
+echo.   >> webpack.config.js
+echo var BUILD_DIR = path.resolve(__dirname, 'src/client/public');   >> webpack.config.js
+echo var APP_DIR = path.resolve(__dirname, 'src/client/app');   >> webpack.config.js
+echo.  >> webpack.config.js
+echo var config = {   >> webpack.config.js
+echo entry: APP_DIR + '/index.jsx',   >> webpack.config.js
+echo  output: {    >> webpack.config.js
+echo  path: BUILD_DIR,          >> webpack.config.js
+echo filename: 'bundle.js'           >> webpack.config.js
+echo },           >> webpack.config.js
+echo.            >> webpack.config.js
+echo  module : {           >> webpack.config.js
+echo   loaders : [          >> webpack.config.js
+echo    {        >> webpack.config.js
+echo      test : /\.jsx?/,        >> webpack.config.js
+echo      include : APP_DIR,            >> webpack.config.js
+echo      loader : 'babel-loader'           >> webpack.config.js
+echo     }           >> webpack.config.js
+echo    ]           >> webpack.config.js
+echo   }          >> webpack.config.js
+echo. >> webpack.config.js
+echo  };          >> webpack.config.js
+echo.  >> webpack.config.js
+echo   module.exports = config; >> webpack.config.js
+rem -----------------------------------------------------------------------------
+echo "create folder src/client/app"
+mkdir src
+cd src
+mkdir client
+cd client
+mkdir app
+cd app
+rem -----------------------------------------------------------------------------
+echo "create index.jsx file to src/client/app"
+echo // Your first reactJS program, modify this code and recompile it >> index.jsx
+echo //use : ^"npm run-script build^" cmd command  >> index.jsx
+echo // to update budle.js file then just refresh your browser web-page >> index.jsx
+echo import React from 'react'; >> index.jsx
+echo import {render} from 'react-dom';  >> index.jsx
+echo.          >> index.jsx
+echo class App extends React.Component {  >> index.jsx
+echo  	render () {  >> index.jsx
+echo  		return ^<p^> Hello React!^</p^>;  >> index.jsx
+echo  	}        >> index.jsx
+echo  }        >> index.jsx
+echo  render(^<App/^>, document.getElementById('app'));  >> index.jsx
+echo console.log('Hello World!'); >> index.jsx
+rem -----------------------------------------------------------------------------
+cd ..
+echo "create index.html to folder src/client"
+rem https://stackoverflow.com/questions/19642622/how-do-i-echo-ascii-art-that-contains-special-characters-in-a-batch-file
+rem http://www.robvanderwoude.com/escapechars.php
+echo ^<html^>      >> index.html
+echo  ^<head^>     >> index.html
+echo    ^<meta charset="utf-8"^>     >> index.html
+echo   ^<title^>React.js using NPM, Babel6 and Webpack^</title^>     >> index.html
+echo  ^</head^>      >> index.html
+echo  ^<body^>      >> index.html
+echo  	^<div id="app" /^>      >> index.html
+echo  	^<script src="public/bundle.js" type="text/javascript"^>^</script^>  >> index.html
+echo  ^</body^>   >> index.html
+echo  ^</html^> >> index.html
+rem -----------------------------------------------------------------------------
+cd..
+cd..
+echo "create .babelrc file "
+echo {  >> .babelrc
+echo  "presets" : ["es2015", "react"]  >> .babelrc
+echo }  >> .babelrc
+rem -----------------------------------------------------------------------------
+echo "execute nmp install command, installing node_modules folder"
+echo "build project, make budle.js file in src/public "
+rem https://ss64.com/nt/cmd.html
+rem start /wait cmd /k npm install / 
+rem npm install /wait npm run-script build
+SET mypath=%~dp0
+echo %mypath:~0,-1%
+rem npm install & npm run-script build & start firefox file:///%mypath%/src/client/index.html & start notepad %mypath%/src/client/app/index.jsx 
+npm install & npm run-script build
+rem npm run-script build
+rem https://stackoverflow.com/questions/3827567/how-to-get-the-path-of-the-batch-script-in-windows
+rem https://serverfault.com/questions/245393/how-do-you-wait-for-an-exe-to-complete-in-batch-file
+rem start /wait cmd /k npm run-script build 
+rem start firefox file:///%mypath%/src/client/index.html
+
+
+
+
+
+
